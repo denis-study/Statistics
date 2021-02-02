3683650bba929b9084366f9e8a61c9d01dd6d084
Denis T <dengertd@gmail.com>
2021-02-02 21:57:22 +0300

    first commit

---
+++ src/main/java/StatsService.java
@@ -0,0 +1,14 @@
+package ru.netology.stats;
+
+public class StatsService {
+    int month = 0; // начинаем с нуля
+        for(
+    int value:values)
+
+    {
+        // на каждой итерации прибавляем единицу
+        // таким образом для первого значения month = 1
+        month = month + 1; // в сокращённой записи: month += 1 или month++
+        // TODO: дальше работаете с value, зная, что номер месяца хранится в month
+    }
+}
